55fc8f251d4d4abcc455d6f72e13ff7423e25aa2
InjuredMuffin2 <k00276349@student.tus.ie>
2023-11-22 10:28:27 +0000

    working on settings

--- Assets/00-MyAssets/Scripts/Settings.cs
+++ Assets/00-MyAssets/Scripts/Settings.cs
@@ -1,41 +1,112 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.Audio; // For audio management
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
+    public GameObject UICore;
+
     // Graphics settings
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    public TMP_InputField framerateInputField;
+
     public int resolutionWidth = 1920;
     public int resolutionHeight = 1080;
     public bool isFullScreen = true;
-    public int textureQuality = 0; // Lower number for higher quality
-    public int antiAliasing = 0; // Options might be 0, 2, 4, 8 etc.
 
     // Audio settings
-    public AudioMixer audioMixer; // Assign in inspector
+    public AudioMixer audioMixer;
+
+    public Slider masterVolumeSlider;
+    public Slider dialogueVolumeSlider;
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
+    public Slider ambienceVolumeSlider;
+
     public float masterVolume = 1.0f;
+    public float dialogueVolume = 1.0f;
     public float musicVolume = 1.0f;
     public float sfxVolume = 1.0f;
+    public float ambienceVolume = 1.0f;
 
-    // Control settings
     public float mouseSensitivity = 1.0f;
     public bool invertYAxis = false;
 
+    private float scaleFactor;
+
     private void Start()
     {
-        SetResolution(resolutionWidth, resolutionHeight, isFullScreen);
+        UICore = GameObject.FindWithTag("UI_Core");
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        framerateInputField.text = Application.targetFrameRate.ToString();
+
+        UpdateUIResolution();
     }
 
-    // Example method to change resolution
-    public void SetResolution(int width, int height, bool fullScreen)
+    public void SetResolution()
     {
-        Screen.SetResolution(width, height, fullScreen);
-        GameObject.FindWithTag("UI_Core");
-        // Set scale factor of UI to adjust for resolution, UI is scaled to 1920x1080 by default
+        Screen.SetResolution(resolutionWidth, resolutionHeight, isFullScreen);
+        UpdateUIResolution();
     }
 
-    // Example method to change master volume
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        resolutionWidth = resolution.width;
+        resolutionHeight = resolution.height;
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+        UpdateUIResolution();
+    }
+
+    private void UpdateUIResolution()
+    {
+        if (resolutionHeight <= 0) resolutionHeight = 1080;
+
+        scaleFactor = resolutionHeight / 1080f;
+        UICore.GetComponent<CanvasScaler>().scaleFactor = scaleFactor;
+    }
+
     public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetFramerate()
+    {
+        int framerate;
+        if (int.TryParse(framerateInputField.text, out framerate))
+        {
+            Application.targetFrameRate = framerate;
+        }
+        else
+        {
+            Debug.LogError("Invalid framerate input");
+        }
     }
 }
